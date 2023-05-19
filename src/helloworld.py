0045ccf4d71281c0afc985e59340b45b6dab7055
Kevin <spamcan0@hotmail.com>
2023-05-19 11:06:08 -0700

    add helloworld.py

---
+++ src/helloworld.py
@@ -0,0 +1,14 @@
+def main():
+    print("Hello World!")
+
+
+main()
+
+
+# echo "# Link_Laptop" >> README.md
+# git init
+# git add README.md
+# git commit -m "first commit"
+# git branch -M main
+# git remote add origin https://github.com/Wanderer027/Link_Laptop.git
+# git push -u origin main
